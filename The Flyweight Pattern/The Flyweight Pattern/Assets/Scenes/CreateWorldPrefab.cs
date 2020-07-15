25f53e6e40a29f9ce12d231bc96e2d3688ab5f83
enJJ <burak.ulusahan@ogr.sakarya.edu.tr>
2020-07-16 00:25:08 +0300

    Create cubes with instanstiate

---
+++ The Flyweight Pattern/The Flyweight Pattern/Assets/Scenes/CreateWorldPrefab.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CreateWorldPrefab : MonoBehaviour{
+
+    public int depth;
+    public int width;
+    public GameObject cube;
+
+    private void Start() {
+        for (int x = 0; x < width; x++) {
+            for (int z = 0; z < depth; z++) {
+                Vector3 pos = new Vector3(x, Mathf.PerlinNoise(x *0.2f, z * 0.2f) * 3, z);
+                GameObject gameObject = Instantiate(cube, pos, Quaternion.identity);
+            }
+        } 
+    }
+
+}
+
